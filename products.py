b639a3041be7dce7f050dab9e5a6a3be37f6d67f
Moszi <niecheeel@gmail.com>
2018-10-11 22:50:47 +0800

    add write function

--- products.py
+++ products.py
@@ -7,3 +7,7 @@ while True:
     products.append([name, price])
 print(products)
 
+with open('products.csv', 'w', encoding = 'utf-8') as file:
+    file.write('Product,Price\n')
+    for p in products:
+        file.write(p[0] + ',' + p[1] + '\n')
