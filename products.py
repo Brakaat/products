969fc8ffd0eccf332874df76493bbf928f5ae876
Moszi <niecheeel@gmail.com>
2018-10-11 23:20:47 +0800

    add read function

--- products.py
+++ products.py
@@ -1,4 +1,14 @@
+# read .csv
 products = []
+with open('products.csv', 'r', encoding = 'utf-8') as file:
+    for line in file:
+        if 'Product,Price' in line:
+            continue
+        name, price = line.strip().split(',')
+        products.append([name, price])
+print(products)
+
+# user input
 while True:
     name = input('Type the products name:')
     if name == 'q':
@@ -7,6 +17,7 @@ while True:
     products.append([name, price])
 print(products)
 
+# write .csv
 with open('products.csv', 'w', encoding = 'utf-8') as file:
     file.write('Product,Price\n')
     for p in products:
