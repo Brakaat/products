8be0401240a0090a32279385551a610870186478
Moszi <niecheeel@gmail.com>
2018-10-17 23:53:51 +0800

    Refactor

--- products.py
+++ products.py
@@ -1,30 +1,42 @@
-# check & read file
-import os #operating system
-
-products = []
-if os.path.isfile('products.csv'): #相對路徑> same folder
-    print('The file is exist')
-    with open('products.csv', 'r', encoding = 'utf-8') as file:
-        for line in file:
-            if 'Product,Price' in line:
-                continue
-            name, price = line.strip().split(',')
-            products.append([name, price])
+# read file
+import os #operating system > find the file authority
+def read_file(filename):
+    products = []
+    with open(filename, 'r', encoding = 'utf-8') as file:
+            for line in file:
+                if 'Product,Price' in line:
+                    continue
+                name, price = line.strip().split(',')
+                products.append([name, price])
     print(products)
-else:
-    print('The file is not exsit')
+    return products
 
 # user input
-while True:
-    name = input('Type the products name:')
-    if name == 'q':
-        break
-    price = input('Price:')
-    products.append([name, price])
-print(products)
+def user_input(products):
+    while True:
+        name = input('Type the products name:')
+        if name == 'q':
+            break
+        price = input('Price:')
+        products.append([name, price])
+    print(products)
+    return products
 
 # write .csv
-with open('products.csv', 'w', encoding = 'utf-8') as file:
-    file.write('Product,Price\n')
-    for p in products:
-        file.write(p[0] + ',' + p[1] + '\n')
+def write_file(filename, products):
+    with open(filename, 'w', encoding = 'utf-8') as file:
+        file.write('Product,Price\n')
+        for p in products:
+            file.write(p[0] + ',' + p[1] + '\n')
+
+def main():
+    filename = 'products.csv'
+    if os.path.isfile(filename): #相對路徑> same folder
+        print('The file is exist')
+        products = read_file(filename)
+    else:
+        print('The file is not exsit')
+    products = user_input(products)
+    write_file(filename, products)
+
+main()
